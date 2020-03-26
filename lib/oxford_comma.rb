a9aaa6a57d493d602e363183656d153bbdfb9974
Joshua Williams <jmw0426@hotmail.com>
2020-03-26 00:06:41 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,14 @@
 def oxford_comma(array)
  
-array = [] 
- 
-if 
+ if array.count == 1 
+  return array
+ elsif array.count == 2 
+  return array.joi("and")
+ else
+  array[-1] = "and #{array[-1]}"
+  return array.joi(",")
+ end  
+end 
 
  
  
