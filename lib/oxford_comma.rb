56c8b60e332ab1589107c2119a07a266d4480c8e
Joshua Williams <jmw0426@hotmail.com>
2020-03-26 00:24:05 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,23 @@
 def oxford_comma(array)
+ 
+ if array.count == 2 
+ 
+  return array.join(" and ")
+ 
+ elsif array.count > 2
+ 
+  array[-1] = "and #{array[-1]}"
+ 
+  return array.join(", ")
+ 
+ else 
+   
+   return array.join
+ 
+ end  
 
-end
+end 
+
+ 
+ 
+ 
