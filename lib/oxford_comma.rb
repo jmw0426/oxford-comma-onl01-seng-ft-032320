8d1320b3fab5ec6d58fc1c49b9424ee1b18a24f2
Joshua Williams <jmw0426@hotmail.com>
2020-03-26 00:03:16 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,22 +2,8 @@ def oxford_comma(array)
  
 array = [] 
  
-array.join do |new_array|
+if 
 
- puts new_array
  
- end
- 
- array = []
- 
- while array > 1 
- 
- array.split("and") do |new_array|
-   
-   puts new_array
- 
- end
- 
- end
  
  
